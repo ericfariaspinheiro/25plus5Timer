d79449efd0fc1c000f44b970928f9196561f97b9
ericfariaspinheiro <ericfariaspinheiro@gmail.com>
2022-04-20 17:41:46 -0300

    music player

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,13 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Pomodoro from "./components/Pomodoro/Pomodoro";
 import Todo from "./components/Todo/Todo";
-import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
 
 export default function App() {
 	return (
-		<>
-			<div id="fullPage">
-				<Header />
-				<Pomodoro />
-				<Todo />	
-			</div>
-			<MusicPlayer />
-		</>
+		<div id="fullPage">
+			<Header />
+			<Pomodoro />
+			<Todo />	
+		</div>
 	);
 }
